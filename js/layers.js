db0293bfe93a31800cff7d12edf12ed9a16eaab5
Ben <66697315+Boxit379@users.noreply.github.com>
2021-11-06 20:15:57 -0600

    canbuymax

--- js/layers.js
+++ js/layers.js
@@ -73,7 +73,6 @@ addLayer("s", {
     resource: "■", // Name of prestige currency
     baseResource: "⬤", // Name of resource prestige is based on
     base: 2,
-    canBuyMax: true,
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
