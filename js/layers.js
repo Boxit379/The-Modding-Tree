25510b461e96e0ad1343ab5ce603cd31f32b1f6d
Ben <66697315+Boxit379@users.noreply.github.com>
2021-11-10 22:23:31 -0700

    aaaaaaa

--- js/layers.js
+++ js/layers.js
@@ -75,7 +75,7 @@ addLayer("s", {
     base: 2,
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
+    exponent: 0.75, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
